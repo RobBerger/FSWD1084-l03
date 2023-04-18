346cb381e6c54557bfd62a4c4c1e2a0ce6f169bf
Robert Berger <robertberger.dev@gmail.com>
2023-04-18 15:00:20 -0400

    ran npm run dev

--- build/app.js
+++ build/app.js
@@ -5,4 +5,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
+function helloWorld(req, res, next) {
+    res.send('Hello World');
+}
+app.use('/', helloWorld);
 app.listen(3000);
