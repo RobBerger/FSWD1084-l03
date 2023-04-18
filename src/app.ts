42aa6dc807cb32de7791a2e2cfec5d64508475e4
Robert Berger <robertberger.dev@gmail.com>
2023-04-18 14:59:05 -0400

    updated app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,11 @@
-import express from 'express'
+import express, { NextFunction, Request, Response } from 'express'
 
 const app = express();
+
+function helloWorld(req: Request, res: Response, next: NextFunction){
+    res.send('Hello World')
+}
+
+app.use('/', helloWorld);
 
 app.listen(3000);
